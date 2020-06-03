f19fc96683a9dfcef3869e40d7ac8ed6aba3531c
Juan Sebastian Mendez <juansebastian.mendezr@gmail.com>
2020-06-03 12:19:48 -0500

    Changed script to process module 2 mutants

--- run.sh
+++ run.sh
@@ -2,17 +2,20 @@
 FILES=./mutants/**/*.apk
 for apk in $FILES
 do
-  echo "Processing $apk..."
-  calabash-android resign $apk
+  MUTANTFOLDER=$(echo $apk| cut -d'/' -f 3)
+  NUM=${MUTANTFOLDER:31}
+
+  if ! (( $NUM % 2 )) ; then 
+    echo "Processing $apk..."
+    calabash-android resign $apk
   
-  RESULT=$(calabash-android run $apk --format html)
+    RESULT=$(calabash-android run $apk --format html)
 
-  if echo "$RESULT" | grep -c "step failed"; then
-
-    if [ ! -d ./results/$apk ]
-      then mkdir -p ./results/$apk
+    if echo "$RESULT" | grep -c "step failed"; then
+      if [ ! -d ./results/$apk ]
+        then mkdir -p ./results/$apk
+      fi
+      echo "$RESULT" >> ./results/$apk/error.html
     fi
-
-    echo "$RESULT" >> ./results/$apk/error.html
   fi
 done

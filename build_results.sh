63f84a3ee36c84492e939a64ac9877dfcb8e62f7
Juan Jose Villegas <juan.villegas@rappi.com>
2020-06-06 11:02:01 -0500

    build report script added

---
+++ build_results.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+shopt -s nullglob
+FILES=./results/mutants/**/*.apk/error.html
+for report in $FILES; do
+
+  MUTANTFOLDER=$(echo $report| cut -d'/' -f 4)
+  NUM=${MUTANTFOLDER:31}
+
+  echo "Mutant # $NUM | [BDT](${report:1})"
+done

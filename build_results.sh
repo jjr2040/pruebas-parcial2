66d90eea7d7a6a6d3b59b7feccd7310db5232044
Juan Jose Villegas <juan.villegas@rappi.com>
2020-06-06 11:05:26 -0500

    Export results to md

--- build_results.sh
+++ build_results.sh
@@ -6,5 +6,5 @@ for report in $FILES; do
   MUTANTFOLDER=$(echo $report| cut -d'/' -f 4)
   NUM=${MUTANTFOLDER:31}
 
-  echo "Mutant # $NUM | [BDT](${report:1})"
+  echo "Mutant # $NUM | [BDT](${report:1})" >> errors_found.md
 done
